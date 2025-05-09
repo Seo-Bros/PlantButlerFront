1d9b600a50fb4d8c6607e438febdbd9ad844484d
서성진 <tjtjdwls0614@gmail.com>
2025-05-09 11:57:47 +0900

    feat: 페이지 컴포넌트 라우터 연결 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import MyPlant from './pages/MyPlant';
+import AddPlant from './pages/AddPlant';
+import InformationPlant from './pages/InformationPlant';
+import MyProfile from './pages/MyProfile';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/myplant' element={<MyPlant />} />
+        <Route path='/addplant' element={<AddPlant />} />
+        <Route path='/informationplant' element={<InformationPlant />} />
+        <Route path='/myprofile' element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
