a83af086152293c51a53200aac13f122761f4dd8
서성진 <tjtjdwls0614@gmail.com>
2025-05-16 15:10:21 +0900

    feat: 네비게이션 구현 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Routes, Route} from 'react-router-dom';
+
 import { ROUTES } from './constants/routes';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,18 +9,24 @@ import AddPlant from './pages/AddPlant';
 import InformationPlant from './pages/InformationPlant';
 import MyProfile from './pages/MyProfile';
 
+import Navigation from './components/Navigation';
+
 
 function App() {
 
   return (
-    <Routes>
-      <Route path={ROUTES.Home} element={<Home />} />
-      <Route path={ROUTES.Login} element={<Login />} />
-      <Route path={ROUTES.MyPlant} element={<MyPlant />} />
-      <Route path={ROUTES.AddPlant} element={<AddPlant />} />
-      <Route path={ROUTES.InformationPlant} element={<InformationPlant />} />
-      <Route path={ROUTES.MyProfile} element={<MyProfile />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path={ROUTES.Home} element={<Home />} />
+        <Route path={ROUTES.Login} element={<Login />} />
+        <Route path={ROUTES.MyPlant} element={<MyPlant />} />
+        <Route path={ROUTES.AddPlant} element={<AddPlant />} />
+        <Route path={ROUTES.InformationPlant} element={<InformationPlant />} />
+        <Route path={ROUTES.MyProfile} element={<MyProfile />} />
+      </Routes>
+      <Navigation />
+    </>
+
   )
 }
 
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,5 @@
+export default function Navigation() {
+  return(
+    <div>네비게이션</div>
+  )
+}
